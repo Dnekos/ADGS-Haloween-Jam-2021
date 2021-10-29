df9f92a4859f1a075b4a5da4b1ba2ff695ff7cac
Demetrius Nekos <dnekos2462@gmail.com>
2021-10-29 16:54:43 +0100

    Candle spawns ghosts when snuffed

--- Assets/Candle.cs
+++ Assets/Candle.cs
@@ -7,6 +7,14 @@ public class Candle : MonoBehaviour
 {
 
 	CircleCollider2D LitArea;
+	[Header("Ghost Spawning")]
+	[SerializeField, Tooltip("Amount of ghosts that spawn when candle goes out")]
+	int GhostSpawns = 2;
+	[SerializeField, Tooltip("Radius of which ghosts wander from candle")]
+	float WanderRadius = 3;
+	[SerializeField]
+	GameObject ghostPrefab;
+
 
 	// Start is called before the first frame update
 	void Start()
@@ -24,10 +32,24 @@ public class Candle : MonoBehaviour
 		Debug.Log("collide");
 		if (collision.transform.tag == "Player")// && Player.getComponent<Movement>().blowingout)
 		{
-			gameObject.layer = 0; // place the object out of the GhostWall layer
-			// define area of the graph to update, so we dont update the whole level
-			Bounds litbounds = new Bounds(transform.position, Vector3.one * LitArea.radius); 
-			AstarPath.active.UpdateGraphs(litbounds); // update the graph
+			SnuffCandle();
+		}
+	}
+
+	void SnuffCandle()
+	{
+		gameObject.layer = 0; // place the object out of the GhostWall layer
+		// define area of the graph to update, so we dont update the whole level
+		Bounds litbounds = new Bounds(transform.position, Vector3.one * LitArea.radius);
+		AstarPath.active.UpdateGraphs(litbounds); // update the graph
+		Destroy(transform.GetChild(0).gameObject); // delete visual indicator
+
+		for (int i = 0; i < GhostSpawns; i++)
+		{
+			Vector2 spawnpoint = Random.insideUnitCircle.normalized * AstarPath.active.data.gridGraph.depth * 0.5f;
+			GhostBrain newghost = Instantiate(ghostPrefab, spawnpoint, transform.rotation).GetComponent<GhostBrain>();
+			newghost.StartPos = transform.position;
+			newghost.WanderRadius = WanderRadius;
 		}
 	}
 }
