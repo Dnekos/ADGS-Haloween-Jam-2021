d41be3e18b28e6c8f8c657473418df670cebe64d
Sam Collier <sam@samcollier.tech>
2021-10-30 03:04:53 +0100

    Shoot the fireball

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -3,21 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class PlayerCombat : MonoBehaviour
 {
     [SerializeField] private GameObject youDiedUI;
-    
-    void Die()
+    [SerializeField] private GameObject fireBall;
+
+    public void GenerateFireBall()
     {
-        Destroy(gameObject);
-        Instantiate(youDiedUI);
+        Instantiate(fireBall,transform); 
+    }
+
+    private void Start()
+    {
+        GenerateFireBall();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Enemy"))
             Die();
+    }   
+    void Die()
+    {
+        Destroy(gameObject);
+        Instantiate(youDiedUI);
     }
+
 }
