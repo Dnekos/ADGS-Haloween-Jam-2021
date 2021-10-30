a7337cd53f71cff0d5bad1ddf0980d617a3f672b
SAM COLLIER <2100259@uad.ac.uk>
2021-10-30 12:58:39 +0100

    Fireball shoots as we expect

---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FollowMouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Mouse.current.position.ReadValue();
+    }
+}
--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -5,20 +5,35 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerCombat : MonoBehaviour
 {
     [SerializeField] private GameObject youDiedUI;
     [SerializeField] private GameObject fireBall;
+    [SerializeField] private float projectileSpeed = .1f;
+    [SerializeField] private GameObject reticle;
 
-    public void GenerateFireBall()
+    private GameObject projectile;
+    private Vector2 launchDir;
+
+    public void OnFire(InputAction.CallbackContext context)
     {
-        Instantiate(fireBall,transform); 
+        if(context.ReadValue<float>() != 1f)
+            return;
+        Destroy(projectile);
+        projectile = Instantiate(fireBall, transform.position, Quaternion.identity);
+        Camera cam = Camera.main;
+        Vector2 mousePosWorld = cam.ScreenToWorldPoint(reticle.transform.position);
+        launchDir = (mousePosWorld - (Vector2)projectile.transform.position);
     }
 
-    private void Start()
+    private void FixedUpdate()
     {
-        GenerateFireBall();
+        if (projectile != null)
+        {
+            projectile.transform.position += (Vector3)(launchDir * projectileSpeed);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
