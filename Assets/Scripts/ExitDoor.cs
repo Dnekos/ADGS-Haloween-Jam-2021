a3f17e3ce0213162a17cc8b0ca3418dd4bfbd108
Demetrius Nekos <dnekos2462@gmail.com>
2021-10-31 15:41:20 +0000

    Blackout script added, camera not giving errors on death

--- Assets/Scripts/ExitDoor.cs
+++ Assets/Scripts/ExitDoor.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class ExitDoor : MonoBehaviour
 {
+	[SerializeField]
+	GameObject Blackout;
+
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.tag == "Player")
@@ -14,7 +17,7 @@ public class ExitDoor : MonoBehaviour
 					return; // dont get snuffed if any other candles are lit
 			
 			AudioManager.instance.PlaySound("CloseDoor");
-			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+			Instantiate(Blackout).GetComponentInChildren<BlackoutScript>().index = SceneManager.GetActiveScene().buildIndex + 1;
 		}
 	}
 }
