b80517aca11e24288872bbef4823ac5c40801865
Demetrius Nekos <dnekos2462@gmail.com>
2021-10-31 11:00:42 +0000

    Exit Door

---
+++ Assets/ExitDoor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitDoor : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Player")
+		{
+			foreach (Candle candle in FindObjectsOfType<Candle>())
+				if (candle.isLit == true)
+					return; // dont get snuffed if any other candles are lit
+
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}
+	}
+}
